fe5646867263e112d8df8e87e26661d5521aa1b9
Mohamed_jamal <mohamlofy10@gmail.com>
2026-05-13 19:16:21 +0200

    Update api.php

--- SpotLy/routes/api.php
+++ SpotLy/routes/api.php
@@ -12,3 +12,6 @@ Route::post('/accounts/create', [AccountController::class, 'createAccount']);
 
 // مسار تسجيل الحجز الزائف وفحص الحظر التلقائي
 Route::post('/accounts/record-fake-booking', [AccountController::class, 'recordFakeBooking']);
+
+// مسار تسجيل الدخول للتحقق من الهوية وجلب الصلاحيات
+Route::post('/accounts/login', [AccountController::class, 'login']);
