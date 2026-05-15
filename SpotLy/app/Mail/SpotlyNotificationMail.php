fd610ae5f14446f349d15ba9636e4758fad5be60
shadalashter <shadaamajed@gmail.com>
2026-05-15 19:03:43 +0200

    تعديل في ارسال اشعارات معينة للايميل

--- SpotLy/app/Mail/SpotlyNotificationMail.php
+++ SpotLy/app/Mail/SpotlyNotificationMail.php
@@ -3,52 +3,25 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Attachment;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class SpotlyNotificationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
+    public $mailDetails;
+
+    // استقبال بيانات الرسالة (العنوان والنص)
+    public function __construct($mailDetails)
     {
-        //
+        $this->mailDetails = $mailDetails;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    // بناء الرسالة وتوجيهها للقالب
+    public function build()
     {
-        return new Envelope(
-            subject: 'Spotly Notification Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject($this->mailDetails['title'])
+                    ->view('emails.spotly_notification');
     }
 }
